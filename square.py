471382faedd49053b2a3f1a69c7c87332b3bc7da
Егор <145185012+Korporatgit@users.noreply.github.com>
2023-11-21 11:50:55 +0300

    Add files via upload

--- square.py
+++ square.py
@@ -1,21 +1,35 @@
-
+import unittest
 def area(a):
-    '''Принимает число a - сторона квадрата, возвращает площадь квадрата 
-       Параметры:
-                a - сторона квадрата
-        Возврващаемое значение:
-                число - площадь квадрата
-        Пример вызова:
-            print(area(5))'''
     return a * a
 
-
 def perimeter(a):
-    '''Принимает число a - сторона квадрата, возвращает периметр квадрата 
-       Параметры:
-                a - сторона квадрата
-        Возврващаемое значение:
-                число - периметр квадрата
-        Пример вызова:
-            print(area(5))'''
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+    def test_zelo_mul1(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_square_mul2(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_square_mul3(self):
+        res = area(15)
+        self.assertEqual(res, 225)
+
+    def test_square_mul4(self):
+        res = area(1.2)
+        self.assertEqual(res, 1.44)
+
+    def test_square_mul5(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_square_mul6(self):
+        res = perimeter(1.2)
+        self.assertEqual(res, 4.8)
+
+    def test_square_mul7(self):
+        res = perimeter(100)
+        self.assertEqual(res, 400)
