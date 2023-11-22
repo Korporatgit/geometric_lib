ab06dda17891b9550467b66b28e90b42a625502d
Егор <145185012+Korporatgit@users.noreply.github.com>
2023-11-22 14:06:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,29 +3,29 @@ import square
 
 class SquareTestCase(unittest.TestCase):
     def test_zelo_mul1(self):
-        res = area(0)
+        res = square.area(0)
         self.assertEqual(res, 0)
 
     def test_square_mul2(self):
-        res = area(10)
+        res = square.area(10)
         self.assertEqual(res, 100)
 
     def test_square_mul3(self):
-        res = area(15)
+        res = square.area(15)
         self.assertEqual(res, 225)
 
     def test_square_mul4(self):
-        res = area(1.2)
+        res = square.area(1.2)
         self.assertEqual(res, 1.44)
 
     def test_square_mul5(self):
-        res = perimeter(0)
+        res = square.perimeter(0)
         self.assertEqual(res, 0)
 
     def test_square_mul6(self):
-        res = perimeter(1.2)
+        res = square.perimeter(1.2)
         self.assertEqual(res, 4.8)
 
     def test_square_mul7(self):
-        res = perimeter(100)
+        res = square.perimeter(100)
         self.assertEqual(res, 400)
